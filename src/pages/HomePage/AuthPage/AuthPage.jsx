7aff501208edec77cdcaafaabd9adf0106fcfefc
RAJABIBRAZ <akilkil5@gmail.com>
2023-12-07 15:27:54 +0300

    modified:   src/pages/HomePage/AuthPage/AuthPage.jsx

--- src/pages/HomePage/AuthPage/AuthPage.jsx
+++ src/pages/HomePage/AuthPage/AuthPage.jsx
@@ -5,13 +5,13 @@ const AuthPage = () => {
   return(
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
-          <Flex>
+          <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
             {/*left hand side */}
           <Box display={{base: "none", md: "block"}}>
             <Image src= "/auth.png" h={650} alt='phone img' />
           </Box>
 
-          {/* Right hand-side */}
+          {/* Right hand-side */} 
           <VStack spacing={4} align={"stretch"}>
             <AuthForm />
             <Box textAlign={"center"}>Get the app</Box>
